f89770a39657a4ad6b404f1f6314434010d2c8a9
Mamello <mmoshoeshoe@webelieve.co.za>
2024-10-09 12:35:57 +0200

    add top page toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from '../app/components/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+     CommonModule,
+     RouterOutlet,
+     ToolbarComponent 
+    ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
